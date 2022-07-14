885d721079ae0024516fba9401abdded2c03601e
naingyu <sktmdgus1212@naver.com>
2022-07-14 21:36:56 +0900

    local db를 통한 graphql 실습

--- movieQL/db/db.js
+++ movieQL/db/db.js
@@ -1,27 +1,51 @@
-export const people = [
+let movies = [
     {
         id: 1,
-        name: "NIG",
-        age: 24,
-        gender: "male",
+        name: "star wars",
+        score: 2
     },
     {
         id: 2,
-        name: "NSH",
-        age: 27,
-        gender: "female",
+        name: "범죄도시",
+        score: 2
     },
     {
         id: 3,
-        name: "NTW",
-        age: 60,
-        gender: "male",
+        name: "설국열차",
+        score: 0
     },
     {
         id: 4,
-        name: "NMS",
-        age: 53,
-        gender: "female",
+        name: "마녀",
+        score: 3
     }
 ];
 
+
+export const getMovies = () => movies;
+
+export const getById = id => {
+    const filteredmovies =  movies.filter(movie => movie.id === id);
+    return filteredmovies[0];
+};
+
+export const deleteMovies = id => {
+    const cleanmovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanmovies.length){
+        movies = cleanmovies;
+        return true;
+    }
+    else{
+        return  false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
+}
--- movieQL/resolvers.js
+++ movieQL/resolvers.js
@@ -1,10 +1,14 @@
-import { people } from "./db/db";
+import { getMovies, getById, addMovie, deleteMovies } from "./db/db";
 
 const resolvers = {
     Query: {
-        people: () => people
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score),
+        deleteMovies: (_, { id }) => deleteMovies(id)
     }
 }
-
 
 export default resolvers;
